6077c7481d7904b1564bd973bb4e77d3517b1793
Magdalena Wlazlo <magdawlazlo@gmail.com.pl>
2020-01-18 11:26:44 +0100

    implementation of rockedmethod

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,7 +1,9 @@
-
+ 
 public class SomeProgram { 
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
-
-System.out.println("We will be Git experts, soon.");    }
+         System.out.println("We will be Git experts, soon.");    }
+public void  public void manage-rocket() {
+    // TODO implement
+} 
 }
